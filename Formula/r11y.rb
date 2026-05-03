1f8531278885016c41310ef1abfbdd3ed952c451
Dan Peddle <dan@flarework.com>
2026-05-03 09:21:50 +0100

    Bump r11y to 1.0.6

--- Formula/r11y.rb
+++ Formula/r11y.rb
@@ -1,20 +1,20 @@
 class R11y < Formula
   desc "Extract readable content from web pages as Markdown"
   homepage "https://github.com/dazld/r11y"
-  version "1.0.5"
+  version "1.0.6"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/dazld/r11y/releases/download/v1.0.5/r11y-macos-arm64"
-      sha256 "aeebea1cd20bd6128d465e416bd882cf0e533daebc5178403d29c25e629d9c68"
+      url "https://github.com/dazld/r11y/releases/download/v1.0.6/r11y-macos-arm64"
+      sha256 "bf3940f01c8acf01204f10031121a90f5b4a089beac7a12a6d7e617fe74a0458"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/dazld/r11y/releases/download/v1.0.5/r11y-linux-amd64"
-      sha256 "f58d2aab988017bfd61327eb52a42ac747b053478d5643cfdafbf2204deadb8b"
+      url "https://github.com/dazld/r11y/releases/download/v1.0.6/r11y-linux-amd64"
+      sha256 "697a5d16b1626ce5dcd18d2e3e5280487843d7824ef27b39459716ac3c927307"
     end
   end
 
